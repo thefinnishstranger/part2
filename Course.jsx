0f7a02625c5f2a0b65a73f646d950ec257038b07
Nikolas Gustavson <140442591+thefinnishstranger@users.noreply.github.com>
2024-05-21 13:07:04 +0300

    Update Course.jsx

--- Course.jsx
+++ Course.jsx
@@ -1,1 +1,55 @@
+const Course = ({ course }) => {
+    return (
+      <div>
+          <Header course={course} />
+        <ul>
+          <Content parts={course.parts} />
+        </ul>
+          <Total  parts={course.parts} />
+      </div>
+    )
+  }
 
+  const Header = ({ course }) => {
+    return (
+      <div>
+        <h1>
+          Web development curriculum
+        </h1>
+        <h2>
+          {course.name}
+        </h2>
+      </div>
+    )
+  }
+  
+  const Part = ({ part, exercises }) => {
+    return (
+      <li>
+        {part} {exercises}
+      </li>
+    )
+  }
+  
+  const Content = ({ parts }) => {
+      return (
+        <div>
+          {parts.map(part => (
+            <Part key={part.id} part={part.name} exercises={part.exercises} />
+          ))}
+        </div>
+      )
+  }
+  
+  const Total = ({ parts }) => {
+    const total = parts.reduce(( acc, part ) => acc + part.exercises, 0)  
+    return (
+        <div>
+          <b>
+            total of {total} exercises
+          </b>
+        </div>
+      )
+    }
+
+export default Course
